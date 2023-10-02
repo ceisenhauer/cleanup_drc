502769ed4a0824d575cd7506cc8f65e2e58f702c
Catherine Eisenhauer <ceisenhauer@users.noreply.github.com>
2023-10-02 15:27:04 +0200

    update

--- data-raw/dict_geo.R
+++ data-raw/dict_geo.R
@@ -1,5 +1,8 @@
 ## code to prepare `dict_geo` dataset goes here
-dict_geo <- rio::import(here::here('data-raw', 'dict_geo.csv'))
+dict_geo <- rio::import(here::here('data-raw', 'dict_geo.csv')) %>%
+  dplyr::rename(reg = .data$prov,
+                zone = .data$zs,
+                reg_zone = .data$prov_zs)
 
 usethis::use_data(dict_geo,
                   internal = TRUE,
